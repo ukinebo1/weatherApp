eb80962a84d80fd7e16df31aed2067e21d9c8039
Ukinebo1 <perfectprecious2022@gmail.com>
2023-11-30 20:50:31 +0100

    created a weather App using Api

---
+++ script.js
@@ -0,0 +1,31 @@
+async function weatherInformation (){
+  try {
+      const name = document.querySelector('#name').value;
+      const location = document.getElementById('location').value;
+
+      const weatherRes = await fetch(`https://api.openweathermap.org/data/2.5/weather?${location}&appid=74ae31218625639a241dda9d0f745d8d`)
+      const weatherResult = await weatherRes.json()
+
+
+      displayWeatherInformation(name, location, weatherResult);
+  } catch (error) {
+      console.log("Error 404")
+  }
+}
+
+function displayWeatherInformation(name, location, weatherResult){
+  const weatherData = document.querySelector('#weatherData')
+  weatherData.innerHTML =`
+  <li>Hello, ${name}!</li>
+    <li>Location: ${location}</li>
+    <li>Temperature: ${weatherResult.temperature}°C</li>
+    <li>Condition: ${weatherResult.condition}</li>
+  `
+}
+
+let button = document.querySelector("#btn")
+button.addEventListener=('click', weatherInformation)
+
+// https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={74ae31218625639a241dda9d0f745d8d}
+
+// `https://api.openweathermap.org/data/2.5/weather?location=${location}&appid=74ae31218625639a241dda9d0f745d8d`
